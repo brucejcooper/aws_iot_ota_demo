4c2b21d537117cb89faaa532eeebc777fa31c2be
Bruce Cooper <bjc@amazon.com>
2019-12-17 08:44:13 +0800

    Code cleanup

--- demo_client.py
+++ demo_client.py
@@ -26,18 +26,6 @@ OTA_CBOR_NUMBEROFBLOCKS_KEY    = "n"
 
 
 
-# For certificate based connection
-myMQTTClient = AWSIoTMQTTClient("myClientID")
-# TODO make sure you replace the endpoint and credentials with your own values
-# For TLS mutual authentication
-myMQTTClient.configureEndpoint("a2mg298nir3kar-ats.iot.ap-southeast-2.amazonaws.com", 8883)
-myMQTTClient.configureCredentials("ca.pem", "a4bbfeb268-private.pem.key", "a4bbfeb268-certificate.pem.crt")
-
-myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-
 def create_client_token():
     '''Creates a token that can be used to correlate a request/response pair'''
     return str(uuid.uuid4())
@@ -53,7 +41,8 @@ class OTAStream:
     The OTA code makes use of CBOR for its message payload format, with very short (1 character) key names
 
     '''
-    def __init__(self, deviceId, streamname, file_id, file_size, blocksize=4096):
+    def __init__(self, mqtt_client, deviceId, streamname, file_id, file_size, blocksize=4096):
+        self.mqtt_client = mqtt_client
         self.file_id = file_id
         self.next_block_id = 0
         self.file_size = file_size
@@ -64,11 +53,11 @@ class OTAStream:
         self.block_future = None
 
     def __enter__(self):
-        myMQTTClient.subscribe(self.data_topic, 0, self._stream_callback)
+        self.mqtt_client.subscribe(self.data_topic, 0, self._stream_callback)
 
     def __exit__(self, type, value, traceback):
-        logging.info("Unsubscribing from stream")
-        myMQTTClient.unsubscribe(self.data_topic)
+        logging.info("Unsubscribing from stream data")
+        self.mqtt_client.unsubscribe(self.data_topic)
 
 
     async def request_block(self, block_id):
@@ -88,7 +77,7 @@ class OTAStream:
 
         self.block_future = asyncio.get_event_loop().create_future()
 
-        myMQTTClient.publish(self.request_topic, binary_payload, 0)
+        self.mqtt_client.publish(self.request_topic, binary_payload, 0)
         file_id, block_id, block_size, block = await self.block_future
         return file_id, block_id, block_size, block,
 
@@ -118,30 +107,24 @@ class OTAStream:
                 # TODO this will try again.  If there's some sort of repeated problem there should be some sort of bail out mechanism.
 
 
-
-
-class IoTJobExecutor:
+class RpcMessageClient:
     ''' 
-    Represents an interface that can be used to exeucte jobs for an OTA 
-    The message format for Jobs is described at https://docs.aws.amazon.com/iot/latest/developerguide/jobs-devices.html
+    Does RPC in a MQTT.  When a message is sent, it adds a 'clientToken' field, which is then used to correlate responses.  Responses are expected on a topic that is the same as the request topic, but with either /accepted or /rejected on the end of it.
+
+    The main job of this class is to make it use the Python asyncio await mechanisms to wait for the response before continuing.  There should only be
+    one message in flight with this approach.
     '''
-    def __init__(self, deviceId):
+    def __init__(self, mqqt_client, deviceId):
+        self.mqtt_client = mqqt_client
         self.deviceId = deviceId
         self.subscription_topic = "$aws/things/{}/jobs/#".format(self.deviceId)
-        self.futures_by_clienttoken = { }
 
-    def req_resp(self, subtopic, body):
-        '''
-        sends body to request_topic.  Sets up an asyncio future such that the callback can decipher what was sent, and then resolve it to make it asyncio compatible
-        '''
-        # Set up a callabck for when the correct response comes in
-        full_topic = "$aws/things/{}/jobs/{}".format(self.deviceId, subtopic)
-        clientToken = create_client_token()
-        f = asyncio.get_event_loop().create_future()
-        self.futures_by_clienttoken[clientToken] = (asyncio.get_event_loop(), f, full_topic)
-        body['clientToken'] = clientToken
-        myMQTTClient.publish(full_topic, json.dumps(body), 0)
-        return f
+    def __enter__(self):
+        self.mqtt_client.subscribe(self.subscription_topic, 1, self._rpc_callback)
+
+    def __exit__(self, type, value, traceback):
+        logging.info("Unsubscribing from JOB RPC responses")
+        self.mqtt_client.unsubscribe(self.subscription_topic)
 
 
     def _rpc_callback(self, client, userdata, msg):
@@ -154,95 +137,123 @@ class IoTJobExecutor:
         logging.info("Received msg on %s: %s", msg.topic, body)
 
         clientToken = body['clientToken']
-        if clientToken in self.futures_by_clienttoken:
-            loop, future, topic = self.futures_by_clienttoken[clientToken]
-
-            if msg.topic == '{}/accepted'.format(topic):
-                loop.call_soon_threadsafe(future.set_result, body)
-            elif msg.topic == '{}/rejected'.format(topic):
+        if clientToken != self.expected_response_token:
+            logging.error("Received message with client token that did not match expectation.  Ignoring")
+        else:        
+            if msg.topic == '{}/accepted'.format(self.call_topic):
+                self.calling_loop.call_soon_threadsafe(self.response_future.set_result, body)
+            elif msg.topic == '{}/rejected'.format(self.call_topic):
                 logging.error("Jobs call rejected: %s", body)
-                loop.call_soon_threadsafe(future.set_exception, Exception(body))
+                self.calling_loop.call_soon_threadsafe(self.response_future.set_exception, Exception(body))
             else:
-                logging.error("Response to RPC call on unexpected topic. Expected %s, got %s", topic, msg.topic)
-                loop.call_soon_threadsafe(future.set_exception, Exception(body))
-            del self.futures_by_clienttoken[clientToken]
-        else:
-            logging.error("Could not find future for %s", clientToken)
+                logging.error("Response to RPC call on unexpected topic. Expected %s, got %s", self.call_topic, msg.topic)
+                self.calling_loop.call_soon_threadsafe(self.response_future.set_exception, Exception(body))
+            self.expected_response_token = None
 
+    async def call(self, subtopic, body):
+        self.call_topic = "$aws/things/{}/jobs/{}".format(self.deviceId, subtopic)
+        self.expected_response_token = create_client_token()
+        self.calling_loop = asyncio.get_event_loop()
+        self.response_future = self.calling_loop.create_future()
+
+        body['clientToken'] = self.expected_response_token
+        self.mqtt_client.publish(self.call_topic, json.dumps(body), 0)
+        response = await self.response_future
+        return response
+
+
+
+
+
+class IoTJobExecutor:
+    ''' 
+    Represents an interface that can be used to exeucte jobs for an OTA 
+    The message format for Jobs is described at https://docs.aws.amazon.com/iot/latest/developerguide/jobs-devices.html
+    '''
+    def __init__(self, mqtt_client, deviceId):
+        self.deviceId = deviceId
+        self.mqtt_client = mqtt_client
+        self.rpc = RpcMessageClient(mqtt_client, deviceId)
 
     async def poll_for_update(self):
         ''' Ask for the "Next Job". The resonse will have an 'execution' element if there is one.  If there is, execute it.'''
-        next_job = await self.req_resp("$next/get", {})
-        logging.info("Got %s", next_job)
-        return 'execution' in next_job
-
-    def __enter__(self):
-        logging.info("Subscribing")
-        myMQTTClient.subscribe(self.subscription_topic, 1, self._rpc_callback)
-
-    def __exit__(self, type, value, traceback):
-        myMQTTClient.unsubscribe(self.subscription_topic)
-
-
+        with self.rpc:
+            next_job = await self.rpc.call("$next/get", {})
+            logging.info("Got %s", next_job)
+            return 'execution' in next_job
 
     async def execute_next_job(self, timeout_mins=None):
         ''' Finds and executes the next IoT core job for the device.  Throws an exception if there is no job'''
         params = {}
 
-        if timeout_mins is not None:
-            params["stepTimeoutInMinutes"] = timeout_mins
-        # Start the "next job", setting it to "IN_PROGRESS" state, with a timeout
-        job_response = await self.req_resp("start-next", params)
+        with self.rpc:
+            if timeout_mins is not None:
+                params["stepTimeoutInMinutes"] = timeout_mins
+            # Start the "next job", setting it to "IN_PROGRESS" state, with a timeout
+            job_response = await self.rpc.call("start-next", params)
 
-        if not 'execution' in job_response:
-            raise Exception("No Job to execute")
-        job_execution = job_response['execution']
+            if not 'execution' in job_response:
+                raise Exception("No Job to execute")
+            job_execution = job_response['execution']
 
-        try:
-            job_id = job_execution['jobId']
-            version = job_execution['versionNumber']
-            job_document = job_execution['jobDocument']
+            try:
+                job_id = job_execution['jobId']
+                version = job_execution['versionNumber']
+                job_document = job_execution['jobDocument']
 
-            # This is AFR OTA specific
-            firmware_file = job_document['afr_ota']['files'][0]
-            fileid = firmware_file['fileid']
-            sig = firmware_file['sig-sha256-ecdsa']
-            file_size = firmware_file['filesize']
+                # This is AFR OTA specific
+                firmware_file = job_document['afr_ota']['files'][0]
+                fileid = firmware_file['fileid']
+                sig = firmware_file['sig-sha256-ecdsa']
+                file_size = firmware_file['filesize']
 
-            streamname = job_document['afr_ota']['streamname']
+                streamname = job_document['afr_ota']['streamname']
 
-            logging.info("Job %s Streaming %s/%d with sig %s. Size is %f", job_id, streamname, fileid, sig, file_size)
-            s = OTAStream(self.deviceId, streamname, fileid, file_size)
-            with s:
-                await s.stream_all_blocks()
+                logging.info("Job %s Streaming %s/%d with sig %s. Size is %f", job_id, streamname, fileid, sig, file_size)
+                s = OTAStream(self.mqtt_client, self.deviceId, streamname, fileid, file_size)
+                with s:
+                    await s.stream_all_blocks()
 
-            # TODO compare supplied signature against the public key / firmware validator on the chip.  Explicitly reject the job if it does not match
+                # TODO compare supplied signature against the public key / firmware validator on the chip.  Explicitly reject the job if it does not match
 
-            # Update the status to SUCCEEDED. This can also be executed mid job to supply status updates (e.g. percentage complete)
-            job_response = await self.req_resp("{}/update".format(job_id), {
-                "status": "SUCCEEDED",
-                "expectedVersion": version,
-            })
-        except:
-            logging.exception("Error processing stream")
-            job_response = await self.req_resp("{}/update".format(job_id), {                
-                "status": "FAILED",
-                "statusDetails": {
-                    "reason": "Put in your excuse here"
-                },
-                "expectedVersion": version,
-            })
-        logging.info("Final job response is %s", job_response)
+                Update the status to SUCCEEDED. This can also be executed mid job to supply status updates (e.g. percentage complete)
+                job_response = await self.rpc.call("{}/update".format(job_id), {
+                    "status": "SUCCEEDED",
+                    "expectedVersion": version,
+                })
+            except:
+                logging.exception("Error processing stream")
+                job_response = await self.rpc.call("{}/update".format(job_id), {                
+                    "status": "FAILED",
+                    "statusDetails": {
+                        "reason": "Put in your excuse here"
+                    },
+                    "expectedVersion": version,
+                })
+            logging.info("Final job response is %s", job_response)
 
         
+
+
+
+# For certificate based connection
+myMQTTClient = AWSIoTMQTTClient("myClientID")
+# TODO make sure you replace the endpoint and credentials with your own values
+# For TLS mutual authentication
+myMQTTClient.configureEndpoint("a2mg298nir3kar-ats.iot.ap-southeast-2.amazonaws.com", 8883)
+myMQTTClient.configureCredentials("ca.pem", "a4bbfeb268-private.pem.key", "a4bbfeb268-certificate.pem.crt")
+
+myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
 
 logging.info("Connecting")
 myMQTTClient.connect()
 
-job_exec = IoTJobExecutor(deviceId = "OTATest")
-with job_exec:
-    asyncio.get_event_loop().run_until_complete(job_exec.execute_next_job())
+job_exec = IoTJobExecutor(myMQTTClient, deviceId = "OTATest")
+asyncio.get_event_loop().run_until_complete(job_exec.execute_next_job())
 
 logging.info("Shutting down")
 myMQTTClient.disconnect()
